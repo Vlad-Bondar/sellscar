53bd32b4547a3f5c62531f6ed4eec7b7ccb2dae7
Vlad-Bondar <vlad.bondar.99@mail.ru>
2020-07-21 13:44:39 +0300

    add validate

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,16 @@
 class Car < ApplicationRecord
     belongs_to :post
+    validates :price, numericality: {greater_than: 0 } presence: :true
+    validates :car_name , presence: :true
+    validates :drivetrain , presence: :true
+    validates :location , presence: :true
+    validates :transmition , presence: :true
+    validates :vin , presence: :true
+    validates :exterior_collor , presence: :true
+    validates :interior_collor , presence: :true
+    validates :body_style , presence: :true
+    validates :engine , presence: :true
+    validates::millage , numericality: {greater_than: 0 } presence: :true
+
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     has_one :car
+    validates :small_description, presence: :true, lenght: {maximum: 200}
+    validates :big_description, presence: :true
 end
